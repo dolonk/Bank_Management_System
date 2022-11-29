abe8c749afcbd2b571a0daba71a542c875b69cd1
dolonk9 <dolonk9@gmail.com>
2022-11-29 13:33:24 +0600

    Member Registration activity connect with runtime Time database

---
+++ app/src/main/java/com/example/swapno_samity/ReadWriteDetails.java
@@ -0,0 +1,116 @@
+package com.example.swapno_samity;
+
+public class ReadWriteDetails {
+    private String id, name, email, dob, address, phoneNumber, idCard, penCard , adarCard, amount, interest;// gender;
+
+    public ReadWriteDetails(String id, String name, String email, String dob, String address, String phoneNumber, String idCard, String penCard, String adarCard, String amount, String interest) {
+        this.id = id;
+        this.name = name;
+        this.dob = dob;
+        this.email = email;
+        //this.gender = gender;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.idCard = idCard;
+        this.penCard = penCard;
+        this.adarCard = adarCard;
+        this.amount = amount;
+        this.interest = interest;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+//    public String getGender() {
+//        return gender;
+//    }
+//
+//    public void setGender(String gender) {
+//        this.gender = gender;
+//    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getIdCard() {
+        return idCard;
+    }
+
+    public void setIdCard(String idCard) {
+        this.idCard = idCard;
+    }
+
+    public String getPenCard() {
+        return penCard;
+    }
+
+    public void setPenCard(String penCard) {
+        this.penCard = penCard;
+    }
+
+    public String getAdarCard() {
+        return adarCard;
+    }
+
+    public void setAdarCard(String adarCard) {
+        this.adarCard = adarCard;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+
+    public String getInterest() {
+        return interest;
+    }
+
+    public void setInterest(String interest) {
+        this.adarCard = interest;
+    }
+}
